5a0c5b7741cba7da0bdc577e00b9259ebd1924a0
tahir-f <tahirf2002@gmail.com>
2024-02-28 11:40:26 +0500

    Initial Setup

---
+++ server.js
@@ -0,0 +1,26 @@
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+//DB setup
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {});
+
+mongoose.connection.on('error', error => console.error(error));
+mongoose.connection.once('open', () => console.log('connected to mongoose'));
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
